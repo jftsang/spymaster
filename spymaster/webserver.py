9abc37bfbdedff60829605848ce45885b35032fd
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2024-01-01 13:04:45 +0000

    Sessions

--- spymaster/webserver.py
+++ spymaster/webserver.py
@@ -7,6 +7,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.websockets import WebSocket, WebSocketDisconnect
+from starlette.middleware.sessions import SessionMiddleware
 
 from .online_player import OnlinePlayer
 from .players import russia
@@ -19,21 +20,33 @@ class GameServer:
         self.games: Dict[str, Spymaster] = {}
         self.online_players: Dict[str, OnlinePlayer] = {}
 
-    def connect_player(self, code: str, websocket: WebSocket) -> OnlinePlayer:
+    def connect_player(self, websocket: WebSocket) -> OnlinePlayer:
         """If the player is already connected, update her websocket.
         Otherwise create a new player.
         """
-        # if code in self.online_players:
-        #     player = self.online_players[code]
-        #     player.websocket = websocket
-        # else:
-        player = OnlinePlayer(code, websocket=websocket)
+        identifier = websocket.query_params["whoami"]
+        is_white = "white" in websocket.query_params
+
+        if identifier in self.online_players:
+            player = self.online_players[identifier]
+            player.websocket = websocket
+        else:
+            player = OnlinePlayer(name=identifier, websocket=websocket, game=None)
+            self.online_players[identifier] = player
+
+            game = Spymaster(white=player if is_white else russia, black=player if not is_white else russia)
+            game_code = game.white.name + game.black.name
+            self.games[game_code] = game
+
+            player.game = game
 
         return player
 
 
 gs = GameServer()
 app = gs.app
+app.add_middleware(SessionMiddleware, secret_key="some-random-string", max_age=None)
+
 templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
 
 app.mount(
